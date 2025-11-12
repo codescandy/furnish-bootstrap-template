f80d45e3d06898a7e3ca0c4cb3f343faaa630e37
anitaparmar26 <adparmar.2010@gmail.com>
2025-11-12 21:12:44 +0530

    updated pages

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,8 +2,8 @@
 
 // Import Bootstrap JS
 import * as bootstrap from 'bootstrap';
-import './js/custom.js';
-import './js/swiper.js'
+import './custom.js';
+import './swiper.js'
 
 // Import SCSS
-import './scss/style.scss';
+import '../scss/style.scss';
